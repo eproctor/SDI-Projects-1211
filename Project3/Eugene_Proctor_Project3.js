4e385771ada721d7ad08afa6be50e0aed5e39a38
Eugene Proctor <eproctor@fullsail.edu>
2012-11-08 17:13:41 -0500

    Added first files to the Repo

---
+++ Project3/Eugene_Proctor_Project3.js
@@ -0,0 +1,76 @@
+//alert("JavaScript works!");
+
+// Eugene Proctor
+// SDI 1211
+// Project 3
+// My Third JavaScript Project
+
+ 
+
+// Global Variables
+ 
+
+var superHero 	=   	/* Travel to the Staduim  */ 				"Ultra Man", 
+	power1		= 		/* Cost to park at the stadium */  			"phaseshift",
+	power2 		=  		/* Who won the game */						"Atoms hard as Diamonds",
+	fightCrime 	=		/* Does Ultra Man fight crime today?*/		true
+;
+
+// Conditional, Argument Boolean, For Loop, Argument Array, Property Array
+
+if (fightCrime === true) {
+	console.log ("Ultra Man will fight crime today.");
+	console.log ("Ultra Man transforms into his Diamond Armor.");}
+	
+	
+
+	else {console.log ("Ultra Man sits on the couch and watch the Adventures on TV");
+		
+//***		While (feetOnCouch === yes); {
+//***		console.log ("Ultra Man's feet are on the couch and he's eating a sandwich. "); 
+		
+//***		} else {fightCrime = true}
+
+			
+			//If (callHisFriends ===) {};
+
+			
+			var callHisFriends = ["Quick Silver", "Mr Big", "Flying Ace", "The Hulk" ];
+			console.log ("Ultra Man called his friends and has " + callHisFriends.length + " friends coming over...")
+			console.log ("His friends are " )
+			for (var i = 0; i < callHisFriends.length; i++){
+			console.log (callHisFriends[i]);
+};
+
+
+// Loop example with an array
+
+//var output = ["argument1", "argument2", "argument3"];
+
+//for (var i = 0; i < 3; i++) {
+
+//console.log("This is a counter loop for argument variables.  The output is = to " + output[i] );
+
+};
+
+// if (attendTheGame === true) {
+//	if (costOfFries <= 2.00) {
+//		costOfFries = costOfFries - 2.00;
+//		console.log("I will purchase 1 small fry.");
+		
+//	} else {
+//	console.log("If the fries cost more than or equal to $" + costOfFries + ", I'm not going to purchase the fries.");
+	
+	
+//	};
+// } else {
+//	if (weather === false) {
+//		console.log("We decided not to " + travelType + " to the game today.");
+//	} else{
+//		costToPark = costToPark + " is the money we saved on parking.";
+//		console.log(costToPark);
+//	}; 
+// };
+ 
+
+
