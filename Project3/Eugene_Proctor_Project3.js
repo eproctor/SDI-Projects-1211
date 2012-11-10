ad4d9adeb2dcf629e1e7dce71b212c1b37458aef
Eugene Proctor <eproctor@fullsail.edu>
2012-11-09 22:49:11 -0500

    Tested While Loop.

--- Project3/Eugene_Proctor_Project3.js
+++ Project3/Eugene_Proctor_Project3.js
@@ -22,55 +22,94 @@ if (fightCrime === true) {
 	console.log ("Ultra Man will fight crime today.");
 	console.log ("Ultra Man transforms into his Diamond Armor.");}
 	
-	
-
-	else {console.log ("Ultra Man sits on the couch and watch the Adventures on TV");
-		
-//***		While (feetOnCouch === yes); {
-//***		console.log ("Ultra Man's feet are on the couch and he's eating a sandwich. "); 
-		
-//***		} else {fightCrime = true}
-
-			
-			//If (callHisFriends ===) {};
-
-			
+		if (fightCrime === true) {
 			var callHisFriends = ["Quick Silver", "Mr Big", "Flying Ace", "The Hulk" ];
-			console.log ("Ultra Man called his friends and has " + callHisFriends.length + " friends coming over...")
+			console.log ("Ultra Man called his friends and his " + callHisFriends.length + " friends are coming to help him...")
 			console.log ("His friends are " )
 			for (var i = 0; i < callHisFriends.length; i++){
 			console.log (callHisFriends[i]);
-};
-
-
-// Loop example with an array
-
-//var output = ["argument1", "argument2", "argument3"];
-
-//for (var i = 0; i < 3; i++) {
-
-//console.log("This is a counter loop for argument variables.  The output is = to " + output[i] );
-
-};
-
-// if (attendTheGame === true) {
-//	if (costOfFries <= 2.00) {
-//		costOfFries = costOfFries - 2.00;
-//		console.log("I will purchase 1 small fry.");
 		
-//	} else {
-//	console.log("If the fries cost more than or equal to $" + costOfFries + ", I'm not going to purchase the fries.");
-	
-	
-//	};
-// } else {
-//	if (weather === false) {
-//		console.log("We decided not to " + travelType + " to the game today.");
-//	} else{
-//		costToPark = costToPark + " is the money we saved on parking.";
-//		console.log(costToPark);
-//	}; 
-// };
- 
+		
+		
+		}
 
+} else {console.log ("Ultra Man sits on the couch and watch the Adventures on TV");
+
+};
+
+// Time it takes for Ultra Man to solve a crime in seconds...
+console.log ("  ");
+
+var timeClock = 10;
+while (timeClock > 0) {
+console.log ("Ultra Man can Phase Shift in " + timeClock + " seconds...");
+timeClock --};
+console.log ("   ");
+
+
+
+var armor = {
+
+	"vest"	:	5,	
+	"glove"	:	10,
+	"shoes"	:	10,
+	"size"	:	function(){
+		var totalArmor = this.vest + this.glove + this.shoes;
+		return totalArmor; 
+	},
+	
+// Mutator Method
+	"setNewSize" : function(newVest) {
+		this.vest = newVest;
+	}
+
+};
+
+// What is Ultra Man's Weakness
+
+var ultraManPower	= {
+
+	power1 				: 	"Phase Shift",
+	power2				: 	"Turn his Atoms into Diamonds",
+	phaseThroughSolids	: 	"yes",
+	phaseThroughGases	:	"yes",
+	phaseThroughLiquids	:	function () {
+		console.log("Ultra Man cannot phase through liquids.  This is his weakness");
+	} 
+	
+};
+
+var key = "power1";
+
+console.log ( "Ultra Man's power is to " + ultraManPower[key] );
+
+console.log ( "Ultra Man's power is to " + ultraManPower.power2 );
+
+// Change a key value
+ultraManPower.power1 = "What is Ultra Man's weakness? ";
+console.log (ultraManPower[key]);
+
+ultraManPower.phaseThroughLiquids();
+
+
+
+//Math - how many help calls are left to resolve
+
+var callsToResolve = function(helpCalls, resolvedCalls) {
+	callsToResolve = helpCalls - resolvedCalls
+	return callsToResolve;
+};
+
+var heroHelp = callsToResolve(30,25)
+console.log ("The number of help calls to resolve is " + callsToResolve);
+
+
+
+
+//Output
+
+console.log("Their are " + armor.size() + " pieces of armor available for any Hero to use. ");
+armor.setNewSize(50);
+console.log("The total Pieces of armor with additional vest is " + armor.size());
+console.log("   ");
 
