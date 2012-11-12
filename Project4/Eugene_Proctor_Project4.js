d8f32407ce8a6c51924a9de7084d6de233e063ba
Eugene Proctor <eproctor@fullsail.edu>
2012-11-12 17:33:09 -0500

    Loaded initial files to depository

---
+++ Project4/Eugene_Proctor_Project4.js
@@ -0,0 +1,21 @@
+// alert("JavaScript works!");
+
+// Number
+// Format a number to use a specific number of decimal places, as for money: 2.1 to 2.10
+
+console.log("Number");
+console.log("Format a number to use a specific number of decimal places, for ex: money: 2.1 to 2.10");
+
+var twoDecimal = function(number) {
+
+console.log (number.toFixed(2));
+};
+
+var newNumber = twoDecimal(123);
+var newNumber = twoDecimal(1);
+var newNumber = twoDecimal(1.2334);
+var newNumber = twoDecimal(1.2384)
+
+// empty space
+console.log("    ");								
+
