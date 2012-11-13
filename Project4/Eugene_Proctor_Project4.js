1e2f65f459706bb78195993cb024b990d8ce5f14
Eugene Proctor <eproctor@fullsail.edu>
2012-11-12 23:25:23 -0500

    Completed and Tested  - Function: Number Above and Below a Number

--- Project4/Eugene_Proctor_Project4.js
+++ Project4/Eugene_Proctor_Project4.js
@@ -9,12 +9,14 @@ console.log("Format a number to use a specific number of decimal places, for ex:
 var twoDecimal = function(number) {
 
 console.log (number.toFixed(2));
+
+
 };
 
 var newNumber = twoDecimal(123);
 var newNumber = twoDecimal(1);
 var newNumber = twoDecimal(1.2334);
-var newNumber = twoDecimal(1.2384)
+var newNumber = twoDecimal(1.2384);
 
 // empty space
 console.log("    ");								
@@ -23,7 +25,20 @@ console.log("    ");
 
 // ...............................
 // Number
-// Format a number to use a specific number of decimal places, as for money: 2.1 to 2.10
+// Display the number above and below a number within a certain percent
+
+
+var topNumber = function(number) {
+
+console.log ("Based on the number " + number + ".  "  +  Math.ceil(number) + " is the next number up.");
+console.log ("Based on the number " + number + ".  "  +  Math.floor(number) + " is the next number down.");
+
+};
+
+var newNumber = topNumber(123.1);
+
+// empty space
+console.log("    ");								
 
 
 
