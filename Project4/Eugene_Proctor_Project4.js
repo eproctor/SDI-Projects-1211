2b33e36654633d90ebae9a48d6291e92301de994
Eugene Proctor <eproctor@fullsail.edu>
2012-11-15 21:34:45 -0500

    Developed and Tested String to uppercase the first letter

--- Project4/Eugene_Proctor_Project4.js
+++ Project4/Eugene_Proctor_Project4.js
@@ -1,5 +1,9 @@
 // alert("JavaScript works!");
 
+
+
+
+//ok
 // Number
 // Format a number to use a specific number of decimal places, as for money: 2.1 to 2.10
 
@@ -75,9 +79,9 @@ console.log ("This is the numerical version of the string " + parseInt(string, 1
 };
 
 var newInput = input("212.5");
+console.log(" ")
 
-
-// ...............................
+// ............................... ok
 
 // Array
 // Find the total value of just the numbers in an array, even if some of the items are not numbers
@@ -89,4 +93,25 @@ var total = [5, 10, 5, 15].reduce(function(a, b) {
 });
 
 console.log (total + " is the total for the array." )
+console.log(" ")
+
+
+
+
+// ...............................ok
+// String
+// Title case a string (split into words, then uppercase the first letter of each word)
+
+var wordOne = "top dog";
+
+
+function capitaliseFirstLetter(string)
+{
+
+    return string.charAt(0).toUpperCase() + string.slice(1) + " " + string.charAt(0).toUpperCase() + string.slice(1) ;
+}
+
+console.log(wordOne.charAt(0).toUpperCase() + wordOne.slice(1,3) + " " + wordOne.charAt(4).toUpperCase() + wordOne.slice(5));
+console.log(" ")
+
 
